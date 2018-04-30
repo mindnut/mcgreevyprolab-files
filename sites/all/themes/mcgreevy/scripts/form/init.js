9d60eeb6a1d0ac666905467233ed58dafa264371
nicholaslue <nicholas.lue@nysed.gov>
2018-04-30 13:06:48 -0400

    remove initial commit

--- sites/all/themes/mcgreevy/scripts/form/init.js
+++
@@ -1,6 +0,0 @@
-   JotForm.init(function(){
-      $('input_10').hint('ex: myname@example.com');
-	
-   });
-
-   document.getElementById("si" + "mple" + "_spc").value = "22055605476958-22055605476958";
